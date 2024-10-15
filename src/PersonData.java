1c515d3c8a93b30a03cb6042e8371e85b7710059
LLParis <sirlondonparis@gmail.com>
2024-10-14 19:44:24 -0700

    Work In Progress

--- src/PersonData.java
+++ src/PersonData.java
@@ -6,13 +6,66 @@
 
 public class PersonData extends HasState 
 {
-    
     protected String lastName;
     protected String firstName;
     protected String address;
     protected String phone;
-    
-    
+
+    public PersonData() {
+
+    }
+
+
+    /**
+     * Constructor with arguments
+     * @param firstName
+     * @param lastName
+     * @param address
+     * @param phone
+     */
+    public PersonData(String firstName, String lastName, String address, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    /**
+     * Accessor for firstName field.
+     * @return firstName of the person
+     */
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    /**
+     * Accessor for lastName field.
+     * @return lastName of the person
+     */
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    /**
+     * Mutator for firstName field.
+     * @return firstName of the person
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Mutator for lastName field.
+     * @return lastName of the person
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * to string method
+     * @return string representation of the object.
+     */
     public String toString()
     {
         //Do not change this implementation
